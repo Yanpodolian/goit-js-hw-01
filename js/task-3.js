deb377a7d67f2da28c6a4ac7087369846b081eca
Yanpodolian <153991280+Yanpodolian@users.noreply.github.com>
2024-04-09 17:06:55 +0200

    3

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,13 @@
-function getElementWidth (content,padding,border) {
-return Number.parseInt (content + (padding * 2) + (border + border))
+function getElementWidth (contentWidth,paddingWidth,borderWidth) {
+   const x = Number.parseInt(contentWidth);
+   const y = Number.parseFloat(paddingWidth);
+   const b = Number.parseInt(borderWidth);
+     return (x + (y * 2) + (b * 2));
 }
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
+
+
+  // як правильно пораховано (y * 2)  чи   (y + y)  ?????
